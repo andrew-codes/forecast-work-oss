d06cca7931d6762dedebfff42cb754b02d056a05
Andrew Smith <andrewsmith@microsoft.com>
2022-01-04 00:42:51 +0000

    query data directly from ADO

--- src/app.py
+++ src/app.py
@@ -1,8 +1,18 @@
 import datetime
+import base64
+import json
+from os import access, environ
+import requests
+from dotenv import load_dotenv
+from glom import glom
+from functools import reduce
+import numpy as np
+from io import StringIO
 from bokeh.models.annotations import Title
-from bokeh.models.widgets.buttons import Button, Toggle
+from bokeh.models.widgets.buttons import Button
 from bokeh.models.widgets.groups import RadioGroup
-from bokeh.models.widgets.inputs import NumericInput
+from bokeh.models.widgets.inputs import FileInput, NumericInput, PasswordInput, TextAreaInput, TextInput
+from bokeh.models.widgets.markups import Div
 
 import pandas as pd
 import yaml
@@ -21,18 +31,63 @@ from bokeh.plotting import figure, show
 from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
 from bokeh.themes import Theme
 
-DATA_FILE_PATH = "Historical Data for Forecasting.csv"
-LAST_DAYS = 180
-SIMULATION_ITEMS = 15
-DATE_TO_BEGIN_WORKING_ON_ITEMS = "2021-01-01"
+load_dotenv()
 
+def split_list(lst, n):  
+    for i in range(0, len(lst), n): 
+        yield lst[i:i + n] 
 
-def datesWithoutTime(item):
-    item["Closed Date"] = datetime.datetime.strptime(
-        item["Closed Date"].strftime("%Y-%m-%d"), "%Y-%m-%d"
-    ).date()
-    return item
+def set_ado_url(attr, old, new):
+    global ado_url
+    ado_url = new
 
+def set_access_token(attr, old, new):
+    global access_token
+    access_token = new
+
+def set_query(attr, old, new):
+    global query
+    query = new
+
+def reduce_work_item_batches(acc, work_item_ids):
+    authorization_value = 'Basic ' + str(base64.b64encode(bytes(":" + access_token, 'utf-8')), 'utf-8')
+    headers = {"Authorization": authorization_value, "Content-Type": "application/json"}
+    work_item_batch_response = requests.post(url=ado_url+"/_apis/wit/workitemsbatch?api-version=6.0", data=json.dumps({"ids": work_item_ids, "fields": ["System.Id", "Microsoft.VSTS.Common.ClosedBy", "Microsoft.VSTS.Common.ClosedDate", "System.WorkItemType"]}),
+    headers=headers,
+    timeout=5)
+    work_items = work_item_batch_response.json()
+    return acc + list(map(lambda work_item: work_item["fields"], list(work_items['value'])))
+
+def query_work_items():
+    global historical_input_data
+    global all_members
+    global team_member_multi_choice_selection
+    if access_token == None:
+        return
+    authorization_value = 'Basic ' + str(base64.b64encode(bytes(":" + access_token, 'utf-8')), 'utf-8')
+    headers = {"Authorization": authorization_value, "Content-Type": "application/json"}
+    query_payload = {
+      "query": query
+    }
+    query_response = requests.post(url=ado_url+"/_apis/wit/wiql?api-version=6.0", 
+                        data=json.dumps(query_payload),
+                        headers=headers,
+                        timeout=5)
+    if query_response.status_code != 200:
+        return
+    work_item_references = list(map(lambda work_item_reference: work_item_reference['id'], query_response.json()["workItems"]))
+    work_item_batches = list(split_list(work_item_references, 200))
+    work_items = list(reduce(reduce_work_item_batches, work_item_batches, []))
+    historical_input_data = (
+        pd.read_json(
+            StringIO(json.dumps(work_items)),
+        )
+        .dropna()
+    )
+    historical_input_data["Microsoft.VSTS.Common.ClosedBy"] = historical_input_data["Microsoft.VSTS.Common.ClosedBy"].apply(lambda row: glom(row, 'displayName'))
+    historical_input_data['Microsoft.VSTS.Common.ClosedDate'] = pd.to_datetime(historical_input_data['Microsoft.VSTS.Common.ClosedDate'], infer_datetime_format=True).dt.date
+    all_members = historical_input_data["Microsoft.VSTS.Common.ClosedBy"].unique().tolist()
+    team_member_multi_choice_selection.options = all_members
 
 def set_forecast_type(attr, old, new):
     global report_type
@@ -69,7 +124,6 @@ def render_graphs():
         return
 
     throughput = compute_throughput()
-
     render_throughput(throughput)
     render_monte_carlo_distribution(throughput)
 
@@ -82,7 +136,8 @@ def render_monte_carlo_distribution(throughput):
         for i in range(simulations)
     ]
     samples = pd.DataFrame(samples, columns=["Items"])
-    distribution = samples.groupby(["Items"]).size().reset_index(name="Frequency")
+    distribution = samples.groupby(
+        ["Items"]).size().reset_index(name="Frequency")
     distribution_figure.vbar(
         distribution["Items"], top=distribution["Frequency"], width=0.5
     )
@@ -97,18 +152,20 @@ def render_monte_carlo_distribution(throughput):
 
 
 def compute_throughput():
-    filtered_data = kanban_data[(kanban_data["Closed By"].isin(selected_members))]
+    filtered_data = historical_input_data[(
+        historical_input_data["Microsoft.VSTS.Common.ClosedBy"].isin(selected_members))]
     throughput = pd.crosstab(
-        filtered_data["Closed Date"], filtered_data["Work Item Type"], colnames=[None]
+        filtered_data["Microsoft.VSTS.Common.ClosedDate"], filtered_data["System.WorkItemType"], colnames=[
+            None]
     ).reset_index()
     throughput["Story Throughput"] = throughput["User Story"]
-    throughput["Defect Throughput"] = throughput["Bug"]
-    throughput["Total Throughput"] = throughput["User Story"] + throughput["Bug"]
+    # throughput["Defect Throughput"] = throughput["Bug"]
+    throughput["Total Throughput"] = throughput["User Story"]
     date_range = pd.date_range(
-        start=throughput["Closed Date"].min(), end=throughput["Closed Date"].max()
+        start=throughput["Microsoft.VSTS.Common.ClosedDate"].min(), end=throughput["Microsoft.VSTS.Common.ClosedDate"].max()
     )
     throughput = (
-        throughput.set_index("Closed Date")
+        throughput.set_index("Microsoft.VSTS.Common.ClosedDate")
         .reindex(date_range)
         .fillna(0)
         .astype(int)
@@ -117,8 +174,8 @@ def compute_throughput():
     throughput_df = pd.DataFrame(
         {
             "All": throughput["Total Throughput"].resample("W-Mon").sum(),
-            "Story": throughput["Story Throughput"].resample("W-Mon").sum(),
-            "Defect": throughput["Defect Throughput"].resample("W-Mon").sum(),
+            "Story": throughput["User Story"].resample("W-Mon").sum(),
+            # "Defect": throughput["Defect Throughput"].resample("W-Mon").sum(),
         }
     ).reset_index()
     return throughput_df
@@ -134,63 +191,63 @@ def select_team_member(attr, old, new):
     global selected_members
     selected_members = new
 
-
-kanban_data = (
-    pd.read_csv(
-        DATA_FILE_PATH,
-        usecols=["Closed Date", "Work Item Type", "Closed By"],
-        parse_dates=["Closed Date"],
-    )
-    .dropna()
-    .transform(datesWithoutTime, "columns")
-)
-kanban_data.head(1)
-
-all_members = kanban_data["Closed By"].unique().tolist()
+query=environ["DEFAULT_QUERY"]
+ado_url = environ["ADO_URL"]
+access_token = environ["ACCESS_TOKEN"]
+historical_input_data = None
+all_members = []
 selected_members = []
 report_type = 0
 simulation_days = 0
 last_days = 0
 
-team_member_multi_choice_selection = MultiChoice(
-    options=all_members, placeholder="Select Team Members"
-)
-team_member_multi_choice_selection.on_change("value", select_team_member)
-
-select_all_team_members_button = Button(label="Select All")
-select_all_team_members_button.on_click(select_all_team_members)
-
-clear_team_members_button = Button(label="Clear")
-clear_team_members_button.on_click(clear_team_members)
-
-buttons = Column(
-    select_all_team_members_button,
-    clear_team_members_button,
-)
-
-team_member_selection_controls = Row(team_member_multi_choice_selection, buttons)
+input_ado_url = TextInput(placeholder="ADO URL", value=ado_url)
+input_ado_url.on_change('value', set_ado_url)
+input_access_token = PasswordInput(placeholder="Access Token with workitem.read scope", value=access_token)
+input_access_token.on_change('value', set_access_token)
+query_button = Button(label="Query Work Items")
+query_button.on_click(query_work_items)
+input_customize_query = TextAreaInput(placeholder="Query", value=query, height=400)
+input_customize_query.on_change('value', set_query)
+input_data_layout = Column(input_ado_url, input_access_token, input_customize_query, query_button)
 
 forecast_type_selection = RadioGroup(
     labels=["How Many Items in Time Frame", "When will N items be completed"]
 )
 forecast_type_selection.on_change("active", set_forecast_type)
 
-simulation_days_input = NumericInput(visible=False, placeholder="Time frame in days")
+simulation_days_input = NumericInput(
+    visible=False, placeholder="Time frame in days")
 simulation_days_input.on_change("value", set_simulation_days)
 how_many_type_options = Row(simulation_days_input)
 when_type_options = Row()
-
 forecast_type_controls = Column(
     forecast_type_selection, how_many_type_options, when_type_options
 )
 
-last_days_input = NumericInput(placeholder="Use the last N days of historical data")
+team_member_multi_choice_selection = MultiChoice(
+    options=all_members, placeholder="Select Team Members"
+)
+team_member_multi_choice_selection.on_change("value", select_team_member)
+select_all_team_members_button = Button(label="Select All")
+select_all_team_members_button.on_click(select_all_team_members)
+clear_team_members_button = Button(label="Clear")
+clear_team_members_button.on_click(clear_team_members)
+buttons = Column(
+    select_all_team_members_button,
+    clear_team_members_button,
+)
+team_member_selection_controls = Row(
+    team_member_multi_choice_selection, buttons)
+
+last_days_input = NumericInput(
+    placeholder="Use the last N days of historical data")
 last_days_input.on_change("value", set_last_days)
 
 run_button = Button(label="Run")
 run_button.on_click(render_graphs)
 
-controls = Column(
+setup_controls_layout = Column(
     forecast_type_controls, last_days_input, team_member_selection_controls, run_button
 )
 
@@ -231,9 +288,9 @@ distribution_hover_tools = HoverTool(
 )
 distribution_figure.add_tools(distribution_hover_tools)
 
-figures = Column(throughput_figure, distribution_figure)
+figures_layout = Column(throughput_figure, distribution_figure)
 
-layout = Row(controls, figures)
+layout = Row(input_data_layout, setup_controls_layout, figures_layout)
 
 curdoc().add_root(layout)
 curdoc().theme = Theme(
