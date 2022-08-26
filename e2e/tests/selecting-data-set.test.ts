692bf0fee7fc0cc4f60b2985632e5228de102371
Andrew Smith <andrewsmith@microsoft.com>
2022-08-26 15:11:34 -0400

    not the only test

--- e2e/tests/selecting-data-set.test.ts
+++ e2e/tests/selecting-data-set.test.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "../configuration/test"
 
-test.only("Given I have not set a data set, when I view the main screen, then the Start button is disabled and the Configuration Pane is open.", async ({
+test("Given I have not set a data set, when I view the main screen, then the Start button is disabled and the Configuration Pane is open.", async ({
   electronApp,
 }) => {
   const firstWindow = await electronApp.firstWindow()
