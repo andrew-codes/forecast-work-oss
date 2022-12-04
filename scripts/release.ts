1724098f492622e7f3f2cc08407fc21c75e45a67
Andrew Smith <andrew@andrew.codes>
2022-12-04 16:49:12 -0500

    testing publishing

--- scripts/release.ts
+++ scripts/release.ts
@@ -23,7 +23,6 @@ const run = async () => {
       appId: "codes.andrew.forecast-work",
       productName: "Forecast Work",
       buildVersion: process.env.VERSION,
-      publish: "never",
       extraMetadata: {
         name: "Forecast Work",
         main: "dist/main/main.js",
